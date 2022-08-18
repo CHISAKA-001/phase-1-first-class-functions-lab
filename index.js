8a1a8468e3ef492298bcc176be2708ff2552f0c5
Edward Chisaka <edwardchisaka@Edwards-MacBook-Pro.local>
2022-08-18 11:21:59 -0700

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function returnFirstTwoDrivers(){
+    return (['Antonia', 'Nuru']);
+}
+
+//should return an array of the last two drivers
+const returnLastTwoDrivers = function returnLastTwoDrivers() {
+    return (['Amari','Mo']);
+}
+
+//selectingDrivers
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+function createFareMultiplier(integer) {
+    return function(fare){
+        return fare * integer;
+    }
+}
+
+//fareDoubler()
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+//selectDifferentDrivers(arrayOfDrivers, function)
+const selectDifferentDrivers = function (arrayOfDrivers, driversToReturn) {
+    return driversToReturn(arrayOfDrivers);
+};
